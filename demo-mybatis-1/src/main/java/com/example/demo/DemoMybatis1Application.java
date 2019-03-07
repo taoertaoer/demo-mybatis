b2c316663ff8387fd81e34af2acdadf7f27a25ef
taoertaoer <15822042457@163.com>
2019-03-07 14:51:00 +0800

    first commit,empty springboot project

---
+++ demo-mybatis-1/src/main/java/com/example/demo/DemoMybatis1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMybatis1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMybatis1Application.class, args);
+	}
+
+}
